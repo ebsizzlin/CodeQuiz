118d45241857b2f5fc92f57c8618b068f8391a92
Ella Thompson <fang98t@gmail.com>
2020-09-29 23:51:04 -0400

    new js files and psuedocoding

---
+++ highscore.js
@@ -0,0 +1,11 @@
+var score
+
+var highscore
+initials []
+scores []
+
+function get scores
+
+function save scores
+
+// tag class / id on html in order to prompt initials and high score
---
+++ script.js
@@ -0,0 +1,13 @@
+function promptQuestion() {}
+if ()   {
+    href
+} else  {
+    // append body
+
+}
+
+// define variables
+// start quiz button click
+// JSON something ? idk what this is yet
+// for loop through the options, add event listener
+// if / else statement for correct answer and wrong answer
